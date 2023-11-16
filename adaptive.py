1226fa8c9f1e02c2344140af8ebb8cfc0e649e13
Preetham Nagesh <preethamnagesh8@gmail.com>
2023-11-16 05:04:21 -0500

    Updated logging message

--- adaptive.py
+++ adaptive.py
@@ -106,7 +106,7 @@ def adaptive_dsd(faulty, connections, num_connections, node_num, lookup):
                 diagnosis = diagnose.diagnose(tested_up, node_num)
                 for i in range(constants.NUM_NODES):
                     if diagnosis[i] == 1:
-                        logger.debug(f"{current_function_name} - Node {i} is faulty")
+                        logger.error(f"{current_function_name} - Node {i} is faulty")
                     else:
                         logger.debug(f"{current_function_name} - Node {i} is not faulty")
             else:
