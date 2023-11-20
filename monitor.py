d4767ac4474212b50ef2d9e9c21bb4fb84da0260
radix007 <47687097+radix007@users.noreply.github.com>
2023-11-20 13:34:03 -0500

    fix variable typo

--- monitor.py
+++ monitor.py
@@ -53,8 +53,9 @@ def update_entropy(entropies):
         entr = entropy.calc_entropy_file(curr['filename'])
         if entr == -1:
             encrp_files += 1
-        elif (entr - curr['entropy']) / curr['entropy'] > constants.ENTROPY_INCREASE_FILE:
+        elif (entr - curr['entropy']) / curr['entropy'] > constants.ENTROPHY_INCREASE_FILE:
             encrp_files += 1
         curr['entropy'] = entr
 
     return encrp_files
+
