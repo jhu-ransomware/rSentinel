d651ee83f9b8593809c46d0a44b36fd0d63f2366
radix007 <47687097+radix007@users.noreply.github.com>
2023-11-20 00:32:53 -0500

    remove raw string for docx and pdf hashes

--- canary.py
+++ canary.py
@@ -66,9 +66,10 @@ def write_config_file(pdf_paths, docx_paths, pdf_hashes, docx_hashes):
         for i, docx_path in enumerate(docx_paths):
             config_file.write(fr"DOCX_PATH_{i}={docx_path}\n")
         for i, pdf_hash in enumerate(pdf_hashes):
-            config_file.write(fr"PDF_HASH_{i}={pdf_hash}\n")
+            config_file.write(f"PDF_HASH_{i}={pdf_hash}\n")
         for i, docx_hash in enumerate(docx_hashes):
-            config_file.write(fr"DOCX_HASH_{i}={docx_hash}\n")
+            config_file.write(f"DOCX_HASH_{i}={docx_hash}\n")
+
 
 def encrypt_config_file():
     with open("config.txt", "rb") as config_file:
