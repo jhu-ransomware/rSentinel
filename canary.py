5efda7c79f5740a66262a763d2f5b152d5ce1850
radix007 <47687097+radix007@users.noreply.github.com>
2023-11-19 22:16:40 -0500

    add logging for debugging of canary

--- canary.py
+++ canary.py
@@ -86,19 +86,26 @@ def decrypt_config_file():
     with open("keys.txt", "r") as key_file:
         encoded_key = key_file.read()
         key = base64.b64decode(encoded_key.encode("utf-8"))  # Decode the Base64-encoded key
+        logging.info(f"Decoded Key: {key}")
+
     with open("config.txt", "rb") as config_file:
         data = config_file.read()
         iv = data[:16]
         ciphertext = data[16:]
+        logging.info(f"IV: {iv}")
+        logging.info(f"Ciphertext: {ciphertext}")
+
     cipher = AES.new(key, AES.MODE_CBC, iv)
     decrypted_data = unpad(cipher.decrypt(ciphertext), AES.block_size)
     config_str = decrypted_data.decode("utf-8")
     config_lines = config_str.split("\n")
+    
     config_dict = {}
     for line in config_lines:
         if "=" in line:
             key, value = line.split("=")
             config_dict[key] = value
+
     return config_dict
 
 
