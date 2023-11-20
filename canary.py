9ef86e7501d66c77e4a173a88e0ac73f96c62a77
radix007 <47687097+radix007@users.noreply.github.com>
2023-11-20 00:57:07 -0500

    fix write issue

--- canary.py
+++ canary.py
@@ -60,15 +60,15 @@ def calculate_sha256(file_path):
     return sha256_hash.hexdigest()
 
 def write_config_file(pdf_paths, docx_paths, pdf_hashes, docx_hashes):
-    with open("config.txt", "wb") as config_file:
+    with open("config.txt", "w") as config_file:
         for i, pdf_path in enumerate(pdf_paths):
-            config_file.write(fr"PDF_PATH_{i}={pdf_path}\n".encode("utf-8"))
+            config_file.write(f"PDF_PATH_{i}={pdf_path}\n")
         for i, docx_path in enumerate(docx_paths):
-            config_file.write(fr"DOCX_PATH_{i}={docx_path}\n".encode("utf-8"))
+            config_file.write(f"DOCX_PATH_{i}={docx_path}\n")
         for i, pdf_hash in enumerate(pdf_hashes):
-            config_file.write(f"PDF_HASH_{i}={pdf_hash}\n".encode("utf-8"))
+            config_file.write(f"PDF_HASH_{i}={pdf_hash}\n")
         for i, docx_hash in enumerate(docx_hashes):
-            config_file.write(f"DOCX_HASH_{i}={docx_hash}\n".encode("utf-8"))
+            config_file.write(f"DOCX_HASH_{i}={docx_hash}\n")
 
 
 def encrypt_config_file():
