b5928c1f983ecea8b19369b695c7a304746de4e9
preethamnagesh8 <preethamnagesh8@Preethams-MacBook-Pro.local>
2023-11-28 17:25:16 -0500

    Added cycle detection to break out of the loop if revisited nodes are visited multiple times

--- diagnose.py
+++ diagnose.py
@@ -8,9 +8,16 @@ def diagnose(tested_up, index):
     current_function_name = inspect.currentframe().f_globals["__name__"] + "." + inspect.currentframe().f_code.co_name
     logger.debug(f"Currently executing: {current_function_name}")
     state_x = [1] * constants.NUM_NODES
+    visited = set()
+    visited_count = 0
 
     ptr = index
     while True:
+        if ptr in visited:
+            visited_count += 1
+            if visited_count >= 5:
+                break
+        visited.add(ptr)
         state_x[ptr] = 0
         ptr = tested_up[ptr]
         if ptr == index or ptr == -1:
