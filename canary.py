f1f16c7545dd16fdf7e7bd944c510e7d2fbb0c3e
radix007 <47687097+radix007@users.noreply.github.com>
2023-12-01 19:14:39 -0500

    modify path canary

--- canary.py
+++ canary.py
@@ -151,14 +151,14 @@ def execute_canary_logic():
         # Specify the full path for file generation
         pdf_paths = [
             os.path.join(
-                generate_random_location(["C:\\Users\\RWareUser\\Downloads", "C:\\Users\\RWareUser\\Desktop", "C:\\Users\\RWareUser\\Documents"]),
+                generate_random_location(["C:\\Users\\rSUser\\Downloads", "C:\\Users\\rSUser\\Desktop", "C:\\Users\\rSUser\\Documents"]),
                 pdf_filename
             ) for pdf_filename in pdf_filenames
         ]
 
         docx_paths = [
             os.path.join(
-                generate_random_location(["C:\\Users\\RWareUser\\Downloads", "C:\\Users\\RWareUser\\Desktop", "C:\\Users\\RWareUser\\Documents"]),
+                generate_random_location(["C:\\Users\\rSUser\\Downloads", "C:\\Users\\rSUser\\Desktop", "C:\\Users\\rSUser\\Documents"]),
                 docx_filename.format(i)
             ) for i in range(6)  # Assuming 6 DOCX files
         ]
