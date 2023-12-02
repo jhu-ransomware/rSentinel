d5a09d68e7478e96d181b738d1201b1ea24d0824
radix007 <47687097+radix007@users.noreply.github.com>
2023-12-01 22:03:41 -0500

    modify fuzzy

--- fuzzy.go
+++ fuzzy.go
@@ -1,123 +1,94 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
 	"fmt"
-	"io/fs"
+	"io/ioutil"
 	"log"
-	"os"
-	"path/filepath"
-	"regexp"
-
-	"github.com/eciavatta/sdhash"
+	"strings"
 )
 
-const defaultPath = `C:\Users\rSUser\Documents`
-
-func calculateSimilarity(filename1, filename2 string) (int, error) {
-	factoryA, err := sdhash.CreateSdbfFromFilename(filename1)
+func decryptConfigFile(keyPath, configPath string) (map[string]string, error) {
+	// Read the encoded key from keys.txt
+	keyBytes, err := ioutil.ReadFile(keyPath)
 	if err != nil {
-		return 0, err
+		return nil, fmt.Errorf("error reading key from keys.txt: %v", err)
 	}
-	sdbfA := factoryA.Compute()
 
-	factoryB, err := sdhash.CreateSdbfFromFilename(filename2)
+	// Decode the Base64-encoded key
+	key, err := base64.StdEncoding.DecodeString(string(keyBytes))
 	if err != nil {
-		return 0, err
+		return nil, fmt.Errorf("error decoding key: %v", err)
 	}
-	sdbfB := factoryB.Compute()
 
-	return sdbfA.Compare(sdbfB), nil
+	// Read the encrypted configuration data from config.txt
+	configBytes, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return nil, fmt.Errorf("error reading config.txt: %v", err)
+	}
+
+	// Extract IV and ciphertext
+	iv := configBytes[:aes.BlockSize]
+	ciphertext := configBytes[aes.BlockSize:]
+
+	// Create a new AES cipher block with the key
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, fmt.Errorf("error creating AES cipher: %v", err)
+	}
+
+	// Create a cipher.NewCBCDecrypter with the AES block and IV
+	mode := cipher.NewCBCDecrypter(block, iv)
+
+	// Decrypt the ciphertext
+	mode.CryptBlocks(ciphertext, ciphertext)
+
+	// Unpad the decrypted data
+	unpaddedData, err := unpad(ciphertext)
+	if err != nil {
+		return nil, fmt.Errorf("error unpadding data: %v", err)
+	}
+
+	// Convert the decrypted data to a string
+	configStr := string(unpaddedData)
+
+	// Parse the configuration string into a map
+	configDict := make(map[string]string)
+	lines := strings.Split(configStr, "\n")
+	for _, line := range lines {
+		if strings.Contains(line, "=") {
+			parts := strings.Split(line, "=")
+			key := strings.TrimSpace(parts[0])
+			value := strings.TrimSpace(parts[1])
+			configDict[key] = value
+		}
+	}
+
+	return configDict, nil
 }
 
-func checkFilesInDirectory(directory string) int {
-	// Check if the directory exists
-	if _, err := os.Stat(directory); os.IsNotExist(err) {
-		log.Printf("Error: Directory %s does not exist.\n", directory)
-		os.Exit(1)
+func unpad(data []byte) ([]byte, error) {
+	padding := int(data[len(data)-1])
+	if padding > aes.BlockSize || padding == 0 {
+		return nil, fmt.Errorf("invalid padding")
 	}
-
-	dissimilarCount := 0
-	totalFileCount := 0
-	// Create a map to store similar file names
-	similarFiles := make(map[string][]string)
-
-	// Define a regular expression to extract base name
-	baseNameRegex := regexp.MustCompile(`^(.+)\.[^.]+$`)
-
-	err := filepath.WalkDir(directory, func(path string, d fs.DirEntry, errWalk error) error {
-		if errWalk != nil {
-			return nil
-		}
-		if !d.IsDir() {
-			baseName := baseNameRegex.ReplaceAllString(d.Name(), "$1")
-
-			similarFiles[baseName] = append(similarFiles[baseName], path)
-
-			fileInfo, errFileInfo := d.Info()
-			if errFileInfo != nil {
-				return nil
-			}
-
-			if fileInfo.Size() < 20*1024 || fileInfo.Size() > 200*1024*1024 {
-				return nil
-			}
-		}
-		return nil
-	})
-
-	if err != nil {
-		return -1
-	}
-
-	for _, files := range similarFiles {
-		if len(files) < 2 {
-			log.Println("Skipping group with less than two files.")
-			continue
-		}
-
-		log.Println("Starting similarity checks...")
-
-		for i, pathA := range files {
-			for j := i + 1; j < len(files); j++ {
-				log.Printf("Checking similarity between %s and %s\n", pathA, files[j])
-
-				similarity, errSimilarity := calculateSimilarity(pathA, files[j])
-				if errSimilarity != nil {
-					log.Println("Error calculating similarity:", errSimilarity)
-					continue
-				}
-
-				totalFileCount++
-
-				if similarity >= 0 && similarity <= 2 {
-					log.Printf("Dissimilarity between %s and %s: %d\n", pathA, files[j], similarity)
-					dissimilarCount++
-				}
-
-				// Check if 200 files have been processed
-				if totalFileCount == 50 {
-					dissimilarityThreshold := 0.99
-					ratio := float64(dissimilarCount) / float64(totalFileCount)
-					log.Printf("Dissimilar Count: %d, Total File Count: %d\n", dissimilarCount, totalFileCount)
-
-					// Return 1 if the dissimilarity ratio is greater than or equal to the threshold
-					if ratio >= dissimilarityThreshold {
-						return 1
-					} else {
-						// Return 0 if the dissimilarity ratio is below the threshold
-						return 0
-					}
-				}
-			}
-		}
-	}
-
-	// Return 0 if the dissimilarity ratio is below the threshold after processing all files
-	return 0
+	return data[:len(data)-padding], nil
 }
 
 func main() {
-	directory := defaultPath
-	result := checkFilesInDirectory(directory)
-	fmt.Printf("Result: %d\n", result)
+	keyPath := "keys.txt"
+	configPath := "config.txt"
+
+	configDict, err := decryptConfigFile(keyPath, configPath)
+	if err != nil {
+		log.Fatalf("Error decrypting config.txt: %v", err)
+	}
+
+	// Print the decrypted configuration
+	fmt.Println("Decrypted Configuration:")
+	for key, value := range configDict {
+		fmt.Printf("%s=%s\n", key, value)
+	}
 }
